5da20c23ceb7b1b53153de3d5d85c9836cd6d69e
lucjasniegota <lucjasniegota@gmail.com>
2017-11-12 10:42:18 +0100

    Zadanie9

--- java-example/src/test/java/Zadanie9.java
+++ java-example/src/test/java/Zadanie9.java
@@ -12,14 +12,15 @@ public class Zadanie9 extends TestBase {
 
 
   @Test
-  public void CountriesTest() {
+  public void countriesTest() {
     driver.get("http://localhost:8090/litecart/admin/?app=countries&doc=countries");
     driver.findElement(By.name("username")).sendKeys("admin");
     driver.findElement(By.name("password")).sendKeys("admin");
     driver.findElement(By.name("login")).click();
 
+
     WebElement calaTabela = driver.findElement(By.className("dataTable"));
- List<WebElement> tabela = calaTabela.findElements(By.xpath(".//td[5]"));
+    List<WebElement> tabela = calaTabela.findElements(By.xpath(".//td[5]"));
     List<String> listaKrajow = new ArrayList<String>();
     List<String> bezSortowania = new ArrayList<String>();
     for (WebElement wiersz : tabela) {
@@ -30,12 +31,13 @@ public class Zadanie9 extends TestBase {
     Collections.sort(listaKrajow);
     assertEquals(bezSortowania, listaKrajow);
 
+
     List<WebElement> tabelaKrajowzeStrefami = calaTabela.findElements(By.className("row"));
     List<String> name = new ArrayList<String>();
-    for (WebElement wiersz2 : tabelaKrajowzeStrefami) {
-      String iloscStref = wiersz2.findElement(By.xpath(".//td[6]")).getAttribute("innerText");
+    for (WebElement wierszKrajow : tabelaKrajowzeStrefami) {
+      String iloscStref = wierszKrajow.findElement(By.xpath(".//td[6]")).getAttribute("innerText");
       if (!(iloscStref.equals("0"))) {
-        name.add(wiersz2.findElement(By.xpath(".//td[5]")).getAttribute("innerText"));
+        name.add(wierszKrajow.findElement(By.xpath(".//td[5]")).getAttribute("innerText"));
       }
     }
     int strefy = name.size();
@@ -52,18 +54,69 @@ public class Zadanie9 extends TestBase {
       }
       Collections.sort(listaStref);
       assertEquals(bezSortowania, listaStref);
-      System.out.println(bezSortowania);
-      System.out.println(listaStref);
       driver.findElement(By.name("cancel")).click();
     }
   }
 
 
   @Test
-  public void litecart2Test() {
+  public void zonesTest() {
     driver.get("http://localhost:8090/litecart/admin/?app=geo_zones&doc=geo_zones");
     driver.findElement(By.name("username")).sendKeys("admin");
     driver.findElement(By.name("password")).sendKeys("admin");
     driver.findElement(By.name("login")).click();
+
+    driver.findElement(By.linkText("Canada")).click();
+    WebElement tableZones = driver.findElement(By.id("table-zones"));
+    List<WebElement> tabelaStrefy = tableZones.findElements(By.xpath(".//td[3]/select/option"));
+    List<String> listaStref = new ArrayList<String>();
+    List<String> bezSortowania = new ArrayList<String>();
+    for (WebElement wiersz : tabelaStrefy) {
+      if (wiersz.isEnabled()){
+      if (wiersz.isSelected()) {
+        String strefa = wiersz.getText();
+        listaStref.add(strefa);
+        bezSortowania.add(strefa);
+      }}
+    }
+    Collections.sort(listaStref);
+    assertEquals(bezSortowania, listaStref);
+    driver.findElement(By.name("cancel")).click();
+
+    driver.findElement(By.linkText("European Union")).click();
+    tableZones = driver.findElement(By.id("table-zones"));
+    tabelaStrefy = tableZones.findElements(By.xpath(".//td[3]/select/option"));
+    listaStref = new ArrayList<String>();
+    bezSortowania = new ArrayList<String>();
+    for (WebElement wiersz : tabelaStrefy) {
+      if (wiersz.isEnabled()){
+      if (wiersz.isSelected()) {
+        String strefa = wiersz.getText();
+        listaStref.add(strefa);
+        bezSortowania.add(strefa);
+      }
+    }}
+    Collections.sort(listaStref);
+    assertEquals(bezSortowania, listaStref);
+    driver.findElement(By.name("cancel")).click();
+
+
+    driver.findElement(By.linkText("United States of America")).click();
+    tableZones = driver.findElement(By.id("table-zones"));
+    tabelaStrefy = tableZones.findElements(By.xpath(".//td[3]/select/option"));
+    listaStref = new ArrayList<String>();
+    bezSortowania = new ArrayList<String>();
+    for (WebElement wiersz : tabelaStrefy) {
+      if (wiersz.isEnabled()){
+      if (wiersz.isSelected()) {
+        String strefa = wiersz.getText();
+        listaStref.add(strefa);
+        bezSortowania.add(strefa);
+      }}
+    }
+    Collections.sort(listaStref);
+    assertEquals(bezSortowania, listaStref);
+
+
   }
 }
