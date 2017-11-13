9ecb036626c09b44b2875e2b75b8085032b611ba
lucjasniegota <lucjasniegota@gmail.com>
2017-11-13 19:56:33 +0100

    poprawione zadanie 7

--- java-example/src/test/java/TestBase.java
+++ java-example/src/test/java/TestBase.java
@@ -1,13 +1,27 @@
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 public class TestBase {
 
   public WebDriver driver;
   public WebDriverWait wait;
+
+  public boolean isElementPresent(By locator) {
+    try {
+      driver.findElement(locator);
+      return true;
+    } catch (Exception ex) {
+      return false;
+    }
+  }
 
   @Before
   public void start() {
