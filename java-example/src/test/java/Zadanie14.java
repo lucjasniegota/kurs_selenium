ae2d82a95bce0c150a87f34a42e84c6360d63b6d
lucjasniegota <lucjasniegota@gmail.com>
2017-11-27 19:09:04 +0100

    zadanie14 z petla

--- java-example/src/test/java/Zadanie14.java
+++ java-example/src/test/java/Zadanie14.java
@@ -20,60 +20,20 @@ public class Zadanie14 extends TestBase {
 
 
     WebElement table = driver.findElement(By.id("content"));
-    List<WebElement> rows = table.findElements(By.tagName("td"));
+    List<WebElement> rows = table.findElements(By.xpath(".//td/a[@target='_blank']"));
 
 
-    rows.get(1).findElement(By.name("iso_code_2")).sendKeys("iso_code_2");
-    rows.get(1).findElement(By.className("fa-external-link")).click();
-    wait.until(numberOfWindowsToBe(2));
-    changeWindow();
-
-    rows.get(2).findElement(By.name("iso_code_3")).sendKeys("iso_code_3");
-    rows.get(2).findElement(By.className("fa-external-link")).click();
-    wait.until(numberOfWindowsToBe(2));
-    changeWindow();
-
-    rows.get(3).findElement(By.name("name")).sendKeys("name");
-    rows.get(4).findElement(By.name("domestic_name")).sendKeys("domestic_name");
-    rows.get(5).findElement(By.name("tax_id_format")).sendKeys("tax_id_format");
-    rows.get(5).findElement(By.className("fa-external-link")).click();
-    wait.until(numberOfWindowsToBe(2));
-    changeWindow();
-
-    rows.get(6).findElement(By.name("address_format")).sendKeys("address_format");
-    rows.get(6).findElement(By.className("fa-external-link")).click();
-    wait.until(numberOfWindowsToBe(2));
-    changeWindow();
-
-    rows.get(7).findElement(By.name("postcode_format")).sendKeys("postcode_format");
-    rows.get(7).findElement(By.className("fa-external-link")).click();
-    wait.until(numberOfWindowsToBe(2));
-    changeWindow();
-
-    rows.get(8).findElement(By.name("currency_code")).sendKeys("currency_code");
-    rows.get(8).findElement(By.className("fa-external-link")).click();
-    wait.until(numberOfWindowsToBe(2));
-    changeWindow();
-
-    rows.get(9).findElement(By.name("phone_code")).sendKeys("phone_code");
-    rows.get(9).findElement(By.className("fa-external-link")).click();
-    wait.until(numberOfWindowsToBe(2));
-    changeWindow();
-
-    rows.get(11).findElement(By.name("zone[code]")).sendKeys("zone[code]");
-    rows.get(12).findElement(By.name("zone[name]")).sendKeys("zone[name]");
-    driver.findElement(By.name("save")).click();
-    
-  }
-
-  private void changeWindow() {
-    String originalWindow = driver.getWindowHandle();
-    Set<String> existingWindows = driver.getWindowHandles();
-    for (String newWindow : existingWindows) {
-      if (!newWindow.equals(originalWindow)) {
-        driver.switchTo().window(newWindow);
-        driver.close();
-        driver.switchTo().window(originalWindow);
+    for (WebElement row : rows) {
+      row.findElement(By.className("fa-external-link")).click();
+      wait.until(numberOfWindowsToBe(2));
+      String originalWindow = driver.getWindowHandle();
+      Set<String> existingWindows = driver.getWindowHandles();
+      for (String newWindow : existingWindows) {
+        if (!newWindow.equals(originalWindow)) {
+          driver.switchTo().window(newWindow);
+          driver.close();
+          driver.switchTo().window(originalWindow);
+        }
       }
     }
   }
