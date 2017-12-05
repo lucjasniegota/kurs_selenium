958d157bc8ec251cad293d32b31e68db0b8ca9c7
lucjasniegota <lucjasniegota@gmail.com>
2017-12-05 14:12:57 +0100

    all

--- java-example/src/test/java/tests/TestBase.java
+++ java-example/src/test/java/tests/TestBase.java
@@ -1,9 +1,10 @@
-import org.junit.After;
-import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+package tests;
+        import org.junit.After;
+        import org.junit.Before;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestBase {
 
@@ -31,4 +32,3 @@ public class TestBase {
     driver = null;
   }
 }
-
--- java-example/src/test/java/tests/Zadanie8.java
+++ java-example/src/test/java/tests/Zadanie8.java
@@ -1,3 +1,5 @@
+package tests;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
